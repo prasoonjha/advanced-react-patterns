77586e32453f2ac7396a6060a30c567c196023b5
Prasoon Jha <prasoonjha69@gmail.com>
2020-07-05 13:20:18 +0530

    resolving wrong animated scle

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -24,6 +24,9 @@ const withClapAnimation = (WrappedComponent) => {
         easing: mojs.easing.out,
       });
 
+      const clap = document.getElementById("clap");
+      clap.style.transform = "scale(1,1)";
+
       const newAnimationTimeline = this.animationTimeline.add([scaleButton]);
       this.setState({ animationTimeline: newAnimationTimeline });
     }
