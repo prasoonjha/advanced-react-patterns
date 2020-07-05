325a0b1e87efb93fd33990111c1f5b4de8711ea1
Prasoon Jha <prasoonjha69@gmail.com>
2020-07-05 13:12:11 +0530

    mojs added; scalebutton animation

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -1,38 +1,132 @@
-import React from "react";
+import React, { Component, useState } from "react";
+import mojs from "mo-js";
+import { generateRandomNumber } from "../utils/generateRandomNumber";
 import styles from "./index.css";
 
-const MediumClap = () => {
+/** ====================================
+ *          🔰HOC
+Higher Order Component for Animation
+==================================== **/
+const withClapAnimation = (WrappedComponent) => {
+  class WithClapAnimation extends Component {
+    animationTimeline = new mojs.Timeline();
+    state = {
+      animationTimeline: this.animationTimeline,
+    };
+
+    componentDidMount() {
+      const tlDuration = 300;
+
+      const scaleButton = new mojs.Html({
+        el: "#clap",
+        duration: tlDuration,
+        scale: { 1.3: 1 },
+        easing: mojs.easing.out,
+      });
+
+      const newAnimationTimeline = this.animationTimeline.add([scaleButton]);
+      this.setState({ animationTimeline: newAnimationTimeline });
+    }
+
+    render() {
+      return (
+        <WrappedComponent
+          animationTimeline={this.state.animationTimeline}
+          {...this.props}
+        />
+      );
+    }
+  }
+
+  WithClapAnimation.displayName = `WithClapAnimation(${getDisplayName(
+    WrappedComponent
+  )})`;
+
+  return WithClapAnimation;
+};
+
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
+}
+
+/** ====================================
+ *      🔰 MediumClap
+==================================== **/
+const initialState = {
+  count: 0,
+  countTotal: generateRandomNumber(500, 10000),
+  isClicked: false,
+};
+
+const MediumClap = ({ animationTimeline }) => {
+  const MAXIMUM_USER_CLAP = 50;
+  const [clapState, setClapState] = useState(initialState);
+  const { count, countTotal, isClicked } = clapState;
+
+  const handleClapClick = () => {
+    // 👉 prop from HOC
+    animationTimeline.replay();
+
+    setClapState({
+      count: Math.min(count + 1, MAXIMUM_USER_CLAP),
+      countTotal: count < MAXIMUM_USER_CLAP ? countTotal + 1 : countTotal,
+      isClicked: true,
+    });
+  };
+
   return (
-    <button className={styles.clap}>
-      <ClapIcon />
-      <ClapCount />
-      <CountTotal />
+    <button id="clap" className={styles.clap} onClick={handleClapClick}>
+      <ClapIcon isClicked={isClicked} />
+      <ClapCount count={count} />
+      <CountTotal countTotal={countTotal} />
     </button>
   );
 };
 
-//subcomponents
-const ClapIcon = () => {
+/** ====================================
+ *      🔰SubComponents
+Smaller Component used by <MediumClap />
+==================================== **/
+
+const ClapIcon = ({ isClicked }) => {
   return (
     <span>
       <svg
+        id="clapIcon"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100.08 125"
-        className={styles.icon}
+        viewBox="-549 338 100.1 125"
+        className={`${styles.icon} ${isClicked && styles.checked}`}
       >
-        <path d="M77.704 12.876a8.1 8.1 0 012.264 4.053c.367-.27.756-.503 1.158-.706.971-1.92.654-4.314-.964-5.891a5.056 5.056 0 00-7.151.091l-.216.222c1.844.174 3.568.927 4.909 2.231zM48.893 26.914c.407.885.687 1.93.791 3.057l16.478-16.928c.63-.648 1.364-1.144 2.145-1.545 1.006-1.93.712-4.367-.925-5.96-2.002-1.948-5.213-1.891-7.155.108L44.722 21.575c2.321 2.261 3.098 3.024 4.171 5.339zM10.041 66.626c-.118-8.864 3.219-17.24 9.396-23.584l18.559-19.064c.727-2.031.497-4.076-.076-5.319-.843-1.817-1.314-2.271-3.55-4.451L13.501 35.645C2.944 46.489 2.253 63.277 11.239 74.94c-.729-2.681-1.161-5.462-1.198-8.314z" />
-        <path d="M21.678 45.206l20.869-21.437c2.237 2.18 2.708 2.634 3.55 4.451.837 1.819.994 5.356-1.607 8.05L32.642 48.514a1.194 1.194 0 00.028 1.689 1.195 1.195 0 001.686-.019l34.047-34.976c1.941-1.999 5.153-2.056 7.155-.108 1.998 1.944 2.03 5.159.089 7.155L50.979 47.584c-.452.464-.437 1.224.038 1.688a1.195 1.195 0 001.689-.009l28.483-29.28a5.055 5.055 0 017.15-.09 5.052 5.052 0 01.097 7.144L59.944 56.308a1.194 1.194 0 00.038 1.688 1.193 1.193 0 001.678-.015l24.66-25.336c1.942-1.995 5.15-2.061 7.15-.113 2.003 1.949 2.043 5.175.101 7.17l-24.675 25.32a1.194 1.194 0 00.038 1.688c.47.457 1.231.453 1.682-.014l14.56-14.973a5.067 5.067 0 017.159-.107 5.052 5.052 0 01.09 7.164L64.792 87.17c-11.576 11.892-30.638 12.153-42.54.569-11.903-11.588-12.149-30.644-.574-42.533" />
+        <path d="M-471.2 366.8c1.2 1.1 1.9 2.6 2.3 4.1.4-.3.8-.5 1.2-.7 1-1.9.7-4.3-1-5.9-2-1.9-5.2-1.9-7.2.1l-.2.2c1.8.1 3.6.9 4.9 2.2zm-28.8 14c.4.9.7 1.9.8 3.1l16.5-16.9c.6-.6 1.4-1.1 2.1-1.5 1-1.9.7-4.4-.9-6-2-1.9-5.2-1.9-7.2.1l-15.5 15.9c2.3 2.2 3.1 3 4.2 5.3zm-38.9 39.7c-.1-8.9 3.2-17.2 9.4-23.6l18.6-19c.7-2 .5-4.1-.1-5.3-.8-1.8-1.3-2.3-3.6-4.5l-20.9 21.4c-10.6 10.8-11.2 27.6-2.3 39.3-.6-2.6-1-5.4-1.1-8.3z" />
+        <path d="M-527.2 399.1l20.9-21.4c2.2 2.2 2.7 2.6 3.5 4.5.8 1.8 1 5.4-1.6 8l-11.8 12.2c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l34-35c1.9-2 5.2-2.1 7.2-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l28.5-29.3c2-2 5.2-2 7.1-.1 2 1.9 2 5.1.1 7.1l-28.5 29.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.4 1.7 0l24.7-25.3c1.9-2 5.1-2.1 7.1-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l14.6-15c2-2 5.2-2 7.2-.1 2 2 2.1 5.2.1 7.2l-27.6 28.4c-11.6 11.9-30.6 12.2-42.5.6-12-11.7-12.2-30.8-.6-42.7m18.1-48.4l-.7 4.9-2.2-4.4m7.6.9l-3.7 3.4 1.2-4.8m5.5 4.7l-4.8 1.6 3.1-3.9" />
       </svg>
     </span>
   );
 };
-
 const ClapCount = ({ count }) => {
-  return <span className={styles.count}>+ {count}</span>;
+  return (
+    <span id="clapCount" className={styles.count}>
+      +{count}
+    </span>
+  );
 };
-
 const CountTotal = ({ countTotal }) => {
-  return <span className={styles.total}>{countTotal}</span>;
+  return (
+    <span id="clapCountTotal" className={styles.total}>
+      {countTotal}
+    </span>
+  );
 };
 
-export default MediumClap;
+/** ====================================
+    *        🔰USAGE
+    Below's how a potential user
+    may consume the component API
+==================================== **/
+
+const Usage = () => {
+  const AnimatedMediumClap = withClapAnimation(MediumClap);
+  return <AnimatedMediumClap />;
+};
+
+export default Usage;
